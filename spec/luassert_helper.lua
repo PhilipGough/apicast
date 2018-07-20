9497febde3984a061ff186965412ea742dc04893
Michal Cichra <michal@cichra.cz>
2018-07-20 15:08:49 +0200

    [spec] implement assert.contains
    match that the expected hash is contained in the actual one
    
    useful for complicated structures we don't want to replicate

--- spec/luassert_helper.lua
+++ spec/luassert_helper.lua
@@ -41,6 +41,7 @@ end
 do -- adding assert.returns_error(error_text, ok, ret) : assert.returns_error('not initialized', _M:call())
   local tostring = tostring
   local tonumber = tonumber
+  local s = require('say')
 
   local function returns_error(state, arguments, level)
     local argc = arguments.n
@@ -58,4 +59,57 @@ do -- adding assert.returns_error(error_text, ok, ret) : assert.returns_error('n
   end
 
   assert:register("assertion", "returns_error", returns_error, "assertion.error.positive", "assertion.error.negative")
+
+  local function contains_left(left, right)
+    if type(left) == 'table' then
+      for k,v in pairs(left) do
+        local same, crumbs = contains_left(v, right[k])
+
+        if not same then
+          crumbs = crumbs or { }
+          table.insert(crumbs,  k)
+          return false, crumbs
+        end
+      end
+
+      return true
+    elseif left == right then
+      return true
+    else
+      return false
+    end
+  end
+
+
+  local function set_failure_message(state, message)
+    if message ~= nil then
+      state.failure_message = message
+    end
+  end
+
+  local function contains(state, arguments, level)
+    local level = (level or 1) + 1
+    local argcnt = arguments.n
+
+    assert(argcnt > 1, s("assertion.internal.argtolittle", { "same", 2, tostring(argcnt) }), level)
+
+    local expected = arguments[1]
+    local actual = arguments[2]
+    assert.is_table(expected)
+    assert.is_table(actual)
+
+    local result, crumbs = contains_left(expected, actual)
+    util.tinsert(arguments, 1, util.tremove(arguments, 2))
+    arguments.fmtargs = arguments.fmtargs or {}
+    arguments.fmtargs[1] = { crumbs = crumbs }
+    arguments.fmtargs[2] = { crumbs = crumbs }
+    set_failure_message(state, arguments[3])
+
+    return result
+  end
+
+  s:set("assertion.contains.positive", "Expected object to contain another.\nPassed in:\n%s\nExpected:\n%s")
+  s:set("assertion.contains.negative", "Expected objects to not contain another.\nPassed in:\n%s\nDid not expect:\n%s")
+
+  assert:register("assertion", "contains", contains, "assertion.contains.positive", "assertion.contains.negative")
 end
