366e0d23bf8e9666ca8c220ba56112bc28219de5
Michal Cichra <michal@cichra.cz>
2018-07-20 17:41:21 +0200

    wip

--- gateway/src/apicast/balancer.lua
+++ gateway/src/apicast/balancer.lua
@@ -1,52 +1,62 @@
 local round_robin = require 'resty.balancer.round_robin'
 local resty_url = require 'resty.url'
-local empty = {}
 
 local _M = { default_balancer = round_robin.new() }
-
-local function get_default_port(upstream_url)
-  local url = resty_url.split(upstream_url) or empty
-  local scheme = url[1] or 'http'
-  return resty_url.default_port(scheme)
-end
 
 local function exit_service_unavailable()
   ngx.status = ngx.HTTP_SERVICE_UNAVAILABLE
   ngx.exit(ngx.status)
 end
 
-function _M.call(_, _, balancer)
-  balancer = balancer or _M.default_balancer
-  local host = ngx.var.proxy_host -- NYI: return to lower frame
-  local peers = balancer:peers(ngx.ctx[host])
+function _M:call(context, bal)
+  if not context then
+    return nil, 'missing context'
+  end
 
+  local host = ngx.var.proxy_host
+  local balancer = bal or _M.default_balancer
+  local upstream = context[host]
+
+  if not upstream then
+    return nil, 'missing upstream'
+  end
+
+  if context[upstream] then
+    return nil, 'already set peer'
+  end
+
+  if host ~= upstream.upstream_name then
+    ngx.log(ngx.ERR, 'upstream name: ', upstream.name, ' does not match proxy host: ', host)
+    return nil, 'upstream host mismatch'
+  end
+
+  local peers = balancer:peers(upstream.servers)
   local peer, err = balancer:select_peer(peers)
 
   if not peer then
     ngx.log(ngx.ERR, 'could not select peer: ', err)
-    return exit_service_unavailable()
+    return exit_service_unavailable(), err
   end
 
   local address, port = peer[1], peer[2]
 
   if not address then
     ngx.log(ngx.ERR, 'peer missing address')
-    return exit_service_unavailable()
-  end
-
-  if not port then
-    port = get_default_port(ngx.var.proxy_pass)
+    return exit_service_unavailable(), 'no address'
   end
 
   local ok
-  ok, err = balancer.balancer.set_current_peer(address, port)
+  ok, err = balancer:set_current_peer(address, port or upstream.uri.port or resty_url.default_port(upstream.uri.scheme))
 
-  if not ok then
+  if ok then
+    -- I wish there would be a nicer way, but unfortunately ngx.exit(ngx.OK) does not
+    -- terminate the current phase handler and will evaluate all remaining balancer phases.
+    context[upstream] = peer
+    return peer
+  else
     ngx.log(ngx.ERR, 'failed to set current backend peer: ', err)
-    return exit_service_unavailable()
+    return exit_service_unavailable(), err
   end
-
-  ngx.log(ngx.INFO, 'balancer set peer ', address, ':', port)
 end
 
 return _M
